07ba48384031b03a6fb85b4c0c7ca80be9ff37f4
christopher <coronac511@gmail.com>
2024-01-30 21:36:32 -0500

    added dark mode functionality

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list")
 
 arrows.forEach((arrow,i)=> {
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
     arrow.addEventListener("click",()=>{
-        movieLists[i].style.transform = `translateX(${ 
-            movieLists[i].computedStyleMap().get("transform")[0].x.value
-        -300}px)`;
+        clickCounter++;
+        if (itemNumber - (4 + clickCounter) >= 0){
+            movieLists[i].style.transform = `translateX(${
+                movieLists[i].computedStyleMap().get("transform")[0].x.value
+                -300}px)`;
+        }else{
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0
+        }
     });
 });
+
+
+//toggle
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movies-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.toggle-ball")
+
+ball.addEventListener("click", ()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+})
